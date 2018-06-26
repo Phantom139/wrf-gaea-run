e228d3fa866f82e98a7ce2060831bb1f36ed6844
Robert Fritzen <rfritzen@phantomdev.net>
2018-06-26 13:18:13 -0600

    Updates
    Removed singleton instance (We required a second) in favor of a better execution route. Added the additional control settings to control.txt and some more functionality has been added.

--- wrf_gaea.py
+++ wrf_gaea.py
@@ -6,82 +6,101 @@
 
 import sys, os, datetime, multiprocessing
 
-# Singleton: Wrapper instance for a single-ly defined class
-class Singleton:
-	__instance = None
-	
-	@staticmethod
-	def instance():
-		if Singleton.__instance == None:
-			Singleton()
-		else:
-			return Singleton.__instance
-	
-	def __init__(self):
-		if(Singleton.__instance != None):
-			printf("Singleton: Error, attempting to init a singleton that already exists")
-		else:
-			Singleton.__instance = self
+# AppSettings: Class responsible for obtaining information from the control file and parsing it to classes that need the information
+class AppSettings():
+    startTime = ""
+    endTime = ""
+    runDays = 0
+    runHours = 0    
+    
+    settings = {}
+
+    def loadSettings(self):
+        with open("control.txt") as f: 
+            for line in f: 
+                tokenized = line.split()
+                if(tokenized[0][0] == '#'):
+                    #Comment line, ignore
+                    print("Comment line: " + line)
+                else:
+                    self.settings[tokenized[0]] = tokenized[1]
+                    print("Applying setting (" + tokenized[0] +"): " + tokenized[1])
+        #Test for program critical settings
+        if(not self.settings):
+            print("Program critical variables missing, check for existence of control.txt, abort.")
+            return False
+        else:
+            return True
+        
+    def fetch(self, key):
+        try:
+            return self.settings[key]
+        except KeyError:
+            print("Key does not exist")
+            return None    
+    
+    def replace(self, key, parms = None):
+        if key == "[run_days]":
+            return str(self.runDays)
+        elif key == "[run_hours]":
+            return str(self.runHours)
+        else:
+            print("Key Error: Target Key " + key + " not present in list")
+            return None
+        
+        
+    def __init__(self):
+        if(self.loadSettings() == False):
+            sys.exit("Failed to load settings, please check for control.txt")
+        
+        self.startTime = datetime.datetime.strptime(self.fetch("starttime"), "%Y%m%d%H")
+        self.runDays = self.fetch("rundays")
+        self.runHours = self.fetch("runhours")
+
+        self.endTime = self.startTime + datetime.timedelta(days=int(self.runDays), hours=int(self.runHours))
 
 # Application: Class responsible for running the program steps.
-class Application(Singleton):
+class Application():
 	# Storage dictionary for program settings
 	settings = {}
-	
-	def loadSettings():
-		with open("control.txt") as f: 
-			for line in f: 
-				tokenized = line.split()
-				if(tokenized[0][0] == '#'):
-					#Comment line, ignore
-				else:
-					self.settings[tokenized[0]] = tokenized[1]
-		#Test for program critical settings
-		if(not self.settings):
-			printf("Program critical variables missing, check for existence of control.txt, abort.")
-			return False
-		else:
-			return True
 			
 	def __init__(self):
-		super().__init__()
-		printf("Initializing WRF Auto-Run Program")
+		print("Initializing WRF Auto-Run Program")
 		#Step 1: Load program settings
-		printf(" 1. Loading program settings")
-		if(loadSettings() == False):
-			sys.exit("Failed at step 1, program critical variables not defined.")
-		printf(" 1. Done.")
+		print(" 1. Loading program settings")
+		settings = AppSettings()
+		print(" 1. Done.")
 		#Step 2: Download CSFV2 Files
-		printf(" 2. Downloading CSFV2 Files")
-		downloads = CSFV2_Fetch()
-		printf(" 2. Done")
+		print(" 2. Downloading CSFV2 Files")
+		downloads = CSFV2_Fetch(settings)
+		print(" 2. Done")
 		#Step 3: Generate WRF Namelist File
-		printf(" 3. Generating namelist files")
-		namelistGenerate = Namelist_Writer()
-		printf(" 3. Done")
+		print(" 3. Generating namelist files")
+		namelistGenerate = Namelist_Writer(settings)
+		print(" 3. Done")
 		#Step 4: Generate GAEA Job Files
-		printf(" 4. Generating GAEA Job Files")
+		print(" 4. Generating GAEA Job Files")
 		
-		printf(" 4. Done")		
+		print(" 4. Done")		
 		#Step 5: Run the preprocessing steps
-		printf(" 5. Run WRF Pre-Processing Steps")
-		preprocessing = Preprocessing_Steps()
-		printf(" 5. Done")
+		print(" 5. Run WRF Pre-Processing Steps")
+		preprocessing = Preprocessing_Steps(settings)
+		print(" 5. Done")
 		#Step 6: Run WRF
-		printf(" 6. Running WRF")
+		print(" 6. Running WRF")
 		
-		printf(" 6. Done")
+		print(" 6. Done")
 		#Step 7: Run postprocessing steps
-		printf(" 7. Running post-processing")
+		print(" 7. Running post-processing")
 		
-		printf(" 7. Done")
+		print(" 7. Done")
 		#Step 8: Cleanup
-		printf(" 8. Cleaning Temporary Files")
+		print(" 8. Cleaning Temporary Files")
 		
-		printf(" 8. Done")		
+		print(" 8. Done")		
 		#Done.
-		printf("All Steps Completed.")
-		printf("Program execution complete.")
+		print("All Steps Completed.")
+		print("Program execution complete.")
 
 #CFSV2_Fetch: Class responsible for downloading and storing the CSFV2 Data
 class CFSV2_Fetch():
@@ -91,15 +110,15 @@ class CFSV2_Fetch():
 	runDays = 1
 	runHours = 1
 
-	def __init__(self):
-		self.cfsDir = Application.instance().settings["cfsDir"]
-		self.startTime = datetime.datetime.strptime(Application.instance().settings["startTime"], "%Y%m%d%H")
-		self.runDays = Application.instance().settings["runDays"]
-		self.runHours = Application.instance().settings["runHours"]
+	def __init__(self, settings):
+		self.cfsDir = settings.fetch("cfsdir")
+		self.startTime = datetime.datetime.strptime(settings.fetch("starttime"), "%Y%m%d%H")
+		self.runDays = settings.fetch("rundays")
+		self.runHours = settings.fetch("runhours")
 		
-		fetchFiles()
+		self.fetchFiles()
 		
-	def fetchFiles():
+	def fetchFiles(self):
 		os.system("mkdir " + self.cfsDir + '/' + str(self.strftime('%Y%m%d%H')))
 	
 		enddate = self.startTime + datetime.timedelta(days=self.runDays, hours=self.runHours)
@@ -114,7 +133,7 @@ class CFSV2_Fetch():
 		pool.close()
 		pool.join()
 	
-	def pooled_download(timeObject):
+	def pooled_download(self, timeObject):
 		prs_lnk = "https://nomads.ncdc.noaa.gov/modeldata/cfsv2_forecast_6-hourly_9mon_pgbf/"
 		flx_lnk = "https://nomads.ncdc.noaa.gov/modeldata/cfsv2_forecast_6-hourly_9mon_flxf/"
 		
@@ -130,36 +149,27 @@ class CFSV2_Fetch():
 		
 # Namelist_Writer: Class responsible for writing the namelist files for WRF
 class Namelist_Writer:
-
+	aSet = None
 	startTime = ""
 	endTime = ""
 	runDays = 0
 	runHours = 0
 	
-	def __init__(self):
-		self.startTime = datetime.datetime.strptime(Application.instance().settings["startTime"], "%Y%m%d%H")
-		self.runDays = Application.instance().settings["runDays"]
-		self.runHours = Application.instance().settings["runHours"]
+	def __init__(self, settings):
+		self.aSet = settings
+		self.startTime = datetime.datetime.strptime(settings.fetch("starttime"), "%Y%m%d%H")
+		self.runDays = settings.fetch("rundays")
+		self.runHours = settings.fetch("runhours")
 		
 		self.endTime = self.startTime + datetime.timedelta(days=self.runDays, hours=self.runHours)
 		
-		generateNamelistInput()
+		self.generateNamelistInput()
 		
-	def generateNamelistInput():
+	def generateNamelistInput(self):
 		with open("namelist.input", 'w') as target_file:
 			with open("namelist.input.template", 'r') as source_file:
 				for line in source_file:
-					newLine = line
-					newLine = newLine.replace("[run_days]", str(self.runDays))
-					newLine = newLine.replace("[run_hours]", str(self.runHours))
-					newLine = newLine.replace("[start_year]", str(self.startTime.year))
-					newLine = newLine.replace("[start_month]", str(self.startTime.month))
-					newLine = newLine.replace("[start_day]", str(self.startTime.day))
-					newLine = newLine.replace("[start_hour]", str(self.startTime.hour))
-					newLine = newLine.replace("[end_year]", str(self.endTime.year))
-					newLine = newLine.replace("[end_month]", str(self.endTime.month))
-					newLine = newLine.replace("[end_day]", str(self.endTime.day))
-					newLine = newLine.replace("[end_hour]", str(self.endTime.hour))					
+					newLine = aSet.replace(line)				
 					target_file.write(newLine)
 	
 # Preprocessing_Steps: Class responsible for running the steps prior to the WRF model
@@ -169,14 +179,14 @@ class Preprocessing_Steps:
 	cfsDir = ""
 	wrfDir = ""
 
-	def __init__(self):
-		self.cfsDir = Application.instance().settings["cfsDir"]
-		self.wrfDir = Application.instance().settings["wrfDir"]
-		self.startTime = Application.instance().settings["startTime"]
+	def __init__(self, settings):
+		self.cfsDir = settings.fetch("cfsdir")
+		self.wrfDir = settings.fetch("wrfdir")
+		self.startTime = settings.fetch("starttime")
 		os.system("module add wrf-3.9.1")
 		os.system("mkdir " + self.wrfDir + '/' + self.startTime[0:8])
 	
-	def run_ungrib():
+	def run_ungrib(self):
 		#Start by symlinking out files from the run folder 
 		os.system("ln -s " + cfsDir + '/' + strTime[0:8] + "/* " + wrfDir + '/' + strTime[0:8])
 		#ungrib.exe needs to run in the data directory
