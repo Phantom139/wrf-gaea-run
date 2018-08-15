2cfb8ad93e9e84175ef1a240b7947a35d69ae968
Robert Fritzen <rfritzen@phantomdev.net>
2018-08-15 16:48:28 -0400

    More adjustments

--- scripts/Tools.py
+++ scripts/Tools.py
@@ -8,6 +8,7 @@ import sys
 import os
 import os.path
 import ApplicationSettings
+import subprocess
 
 #CD: Current Directory management, see https://stackoverflow.com/a/13197763/7537290 for implementation. This is used to maintain the overall OS CWD while allowing embedded changes.
 class cd:
@@ -22,13 +23,13 @@ class cd:
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
 		
-#popen: A wrapped call to the os.popen method to test for the debugging flag.
+#popen: A wrapped call to the subprocess.popen method to test for the debugging flag.
 class popen:
 	def __init__(self, settings, command):
 		if(settings.fetch("debugmode") == '1'):
 			print("D: " + command)
 		else:
-			runCmd = os.popen(command, shell=True).wait()
+			runCmd = subprocess.popen(command, shell=True).wait()
 			self.stored = runCmd.read()
 			
 	def fetch(self):
